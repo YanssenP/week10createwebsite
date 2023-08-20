a83baf143e83893abac9caa041f9d6e3209ac1ef
Yanssen Perianen <yanssenperianen@yanssens-laptop.home>
2023-08-19 23:56:15 -0400

    updates3

--- week10.js
+++ week10.js
@@ -1,3 +1,7 @@
+//Using HTML, Bootstrap, and JavaScript create a single page website that contains the following:
+//A Bootstrap styled table representing your choice of data.
+//A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
+
 let id = 0
 
 document.getElementById('add').addEventListener('click', () => {
@@ -14,16 +18,3 @@ document.getElementById('add').addEventListener('click', () => {
     document.getElementById('new-Home V Away').value = '';
     document.getElementById('new-Match-Date').value = + '';
 });
-
-function createDeleteButton(id) {
-    let btn = document.createElement('button');
-    btn.className = 'btn btn-primary';
-    btn.id = id;
-    btn.innerHTML = 'Delete';
-    btn.onclick = () => {
-        console.log(`Deleting row wiht id: item-${id}`);
-        let elementToDelete = document.getElementById(`item-${id}`)
-        elementToDelete.parentNode.removeChild(elementToDelete);
-    };
-    return btn;
-}
